e3866671024b3f79edda47d669dbe36c38cb188d
Jakub Petrovic <petrovic.jakub@hotmail.com>
2024-03-17 14:29:42 +0100

    apply code analyser sugestion

--- main.go
+++ main.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"fmt"
+	"monkey/repl"
 	"os"
 	"os/user"
-	"monkey/repl"
 )
 
 func main() {
-	user, err := user.Current()
+	usr, err := user.Current()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", usr.Username)
 	fmt.Printf("Feel free to type in commands\n")
 	repl.Start(os.Stdin, os.Stdout)
 }
